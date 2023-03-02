c18e9e04f15b991bc28ad0cc365336fd5b648e30
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-03-02 17:09:26 +1100

    external script imported into houdini

--- project.py
+++ project.py
@@ -1,4 +1,27 @@
+import os
 from PySide2 import QtWidgets
 import hou
+from importlib import reload
 
-hou.addAnimationLayer("test")
+proj = hou.getenv('JOB') + '/'
+
+
+def open_scene(item):
+    print('open hip file')
+    hip_file = proj + item.data()
+    # print hip_file
+    # open hip_file
+    hou.hipFile.load(hip_file)
+
+
+def create_interface():
+    widget = QtWidgets.QLabel(proj)
+    list_widget = QtWidgets.QListWidget()
+
+    for file in os.listdir(proj):
+        list_widget.addItem(file)
+
+    # connect list items to function
+    list_widget.doubleClicked.connect(open_scene)
+
+    return list_widget
