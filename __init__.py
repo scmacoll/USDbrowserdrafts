1f4a4478daa3c15afbc196012b0da4b12b576b59
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-03-04 17:06:20 +1100

    class widget adjustments

--- __init__.py
+++ __init__.py
@@ -1,7 +1,16 @@
 # PYPANEL CODE
+
 # from projectview import project
 # import importlib
 # importlib.reload(project)
 #
 # def onCreateInterface():
 #     return project.create_interface()
+
+
+# from projectview import project
+# import importlib
+# importlib.reload(project)
+#
+# def onCreateInterface():
+#     return project.ProjectManager()
--- project.py
+++ project.py
@@ -1,31 +1,41 @@
 import os
-from PySide2 import QtWidgets
 import hou
-
-proj = hou.getenv('JOB') + '/'
+from PySide2 import QtWidgets
 
 
 class ProjectManager(QtWidgets.QWidget):
     def __init__(self):  # constructor
         super(ProjectManager, self).__init__()
+
+        self.proj = hou.getenv('JOB') + '/'
+
+        # Create widgets
+        self.widget = QtWidgets.QLabel(self.proj)  # label files
+        self.list_widget = QtWidgets.QListWidget()  # create list widget
+
         self.create_interface()
+
+        # Create layout (how widgets will be organised)
+        main_layout = QtWidgets.QVBoxLayout()  # vertical layout
+
+        # Add widgets to layout
+        main_layout.addWidget(self.widget)
+
 
     def open_scene(self, item):
         print('open hip file')
-        hip_file = proj + item.data()
+        hip_file = self.proj + item.data()
         # print hip_file
         # open hip_file
         hou.hipFile.load(hip_file)
 
     def create_interface(self):
         print("creating interface")
-        widget = QtWidgets.QLabel(proj)
-        list_widget = QtWidgets.QListWidget()
 
-        for file in os.listdir(proj):
-            list_widget.addItem(file)
+        for file in os.listdir(self.proj):
+            self.list_widget.addItem(file)
 
         # connect list items to function
-        list_widget.doubleClicked.connect(self.open_scene)
+        self.list_widget.doubleClicked.connect(self.open_scene)
 
-        return list_widget
+        return self.list_widget
