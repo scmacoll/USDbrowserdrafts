9e4b95fb2d1da6e5ebd4d2da319c57e6ed633cc0
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-03-06 16:03:07 +1100

    set project functionality

--- project.py
+++ project.py
@@ -14,26 +14,47 @@ class ProjectManager(QtWidgets.QWidget):
         self.ui = loader.load('/Users/stu/Library/Preferences/houdini/19.5'
                               '/scripts/python/projectview/projectview.ui')
 
-        # Create widgets
-        self.btn = QtWidgets.QPushButton('Click me')
-        self.lbl_title = QtWidgets.QLabel("PROJECT MANAGER")  # label Title
-        self.label = QtWidgets.QLabel(self.proj)  # label files
-        self.list_widget = QtWidgets.QListWidget()  # create list widget
+        # get UI elements (designer)
+        self.set_proj = self.ui.findChild(QtWidgets.QPushButton, 'setproj')
+        self.proj_path = self.ui.findChild(QtWidgets.QLabel, 'projpath')
+        self.proj_name = self.ui.findChild(QtWidgets.QLabel, 'projname')
 
-        self.create_interface()  # run function to create interface
+        # # Create widgets
+        # self.btn = QtWidgets.QPushButton('Click me')
+        # self.lbl_title = QtWidgets.QLabel("PROJECT MANAGER")  # label Title
+        # self.label = QtWidgets.QLabel(self.proj)  # label files
+        # self.list_widget = QtWidgets.QListWidget()  # create list widget
+
+        # create connections
+        self.set_proj.clicked.connect(self.set_project)
+
+        # self.create_interface()
 
         # Create layout (how widgets will be organised)
         main_layout = QtWidgets.QVBoxLayout()  # vertical layout
 
         main_layout.addWidget(self.ui)
 
-        # # Add widgets (manual) to layout
+        # # Add widgets to layout
         # main_layout.addWidget(self.lbl_title)  # label (title) outside list
         # main_layout.addWidget(self.label)  # label outside list
         # main_layout.addWidget(self.list_widget)  # visible list
         # main_layout.addWidget(self.btn)
 
         self.setLayout(main_layout)
+
+    def set_project(self):
+        set_job = hou.ui.selectFile(title='Select Project Folder',
+                                    file_type=hou.fileType.Directory)
+        hou.hscript('setenv JOB=' + set_job)
+        self.proj = hou.getenv('JOB') + '/'
+
+        proj_name = set_job.split('/')[-2]
+        set_job = os.path.dirname(set_job)  # removes "/" @ end of name
+        proj_path = os.path.split(set_job)[0]  # removes project name
+
+        self.proj_name.setText(proj_name)
+        self.proj_path.setText(proj_path + '/')
 
 
     def open_scene(self, item):  # double click to open hip file
