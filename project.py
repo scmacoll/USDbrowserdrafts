d3596c612a0c100bd14274e444e89966ac97abee
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-03-06 14:36:21 +1100

    load qtwidget

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import os
 import hou
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtUiTools
 
 
 class ProjectManager(QtWidgets.QWidget):
@@ -9,28 +9,41 @@ class ProjectManager(QtWidgets.QWidget):
 
         self.proj = hou.getenv('JOB') + '/'
 
+        # Load UI file
+        loader = QtUiTools.QUiLoader()
+        self.ui = loader.load('/Users/stu/Library/Preferences/houdini/19.5'
+                              '/scripts/python/projectview/projectview.ui')
+
         # Create widgets
-        self.widget = QtWidgets.QLabel(self.proj)  # label files
+        self.btn = QtWidgets.QPushButton('Click me')
+        self.lbl_title = QtWidgets.QLabel("PROJECT MANAGER")  # label Title
+        self.label = QtWidgets.QLabel(self.proj)  # label files
         self.list_widget = QtWidgets.QListWidget()  # create list widget
 
-        self.create_interface()
+        self.create_interface()  # run function to create interface
 
         # Create layout (how widgets will be organised)
         main_layout = QtWidgets.QVBoxLayout()  # vertical layout
 
-        # Add widgets to layout
-        main_layout.addWidget(self.label)
-        main_layout.addWidget(self.list_widget)
+        main_layout.addWidget(self.ui)
+
+        # # Add widgets (manual) to layout
+        # main_layout.addWidget(self.lbl_title)  # label (title) outside list
+        # main_layout.addWidget(self.label)  # label outside list
+        # main_layout.addWidget(self.list_widget)  # visible list
+        # main_layout.addWidget(self.btn)
+
+        self.setLayout(main_layout)
 
 
-    def open_scene(self, item):
+    def open_scene(self, item):  # double click to open hip file
         print('open hip file')
         hip_file = self.proj + item.data()
         # print hip_file
         # open hip_file
         hou.hipFile.load(hip_file)
 
-    def create_interface(self):
+    def create_interface(self):  # create list interface
         print("creating interface")
 
         for file in os.listdir(self.proj):
