f54d0a092e369d5e75b864d4aefe4449c3fcf38c
scmacoll <85879687+scmacoll@users.noreply.github.com>
2023-03-15 11:22:05 +1100

    refresh button completed

--- project.py
+++ project.py
@@ -62,6 +62,7 @@ class ProjectManager(QtWidgets.QWidget):
         self.set_proj = self.ui.findChild(QtWidgets.QPushButton, 'setproj')
         self.back_btn = self.ui.findChild(QtWidgets.QPushButton, 'backbtn')
         self.fwd_btn = self.ui.findChild(QtWidgets.QPushButton, 'fwdbtn')
+        self.re_btn = self.ui.findChild(QtWidgets.QPushButton, 'rebtn')
         self.proj_path = self.ui.findChild(QtWidgets.QLabel, 'projpath')
         self.job_path = self.ui.findChild(QtWidgets.QLabel, 'jobpath')
         self.proj_name = self.ui.findChild(QtWidgets.QLabel, 'projname')
@@ -71,16 +72,23 @@ class ProjectManager(QtWidgets.QWidget):
                          '/back.svg'
         back_icon = QtGui.QIcon(back_icon_path)
         self.back_btn.setIcon(back_icon)
+
         fwd_icon_path = '/Users/stu/Documents/3D/QtDesigner/icons/BUTTONS' \
                         '/forward.svg'
         fwd_icon = QtGui.QIcon(fwd_icon_path)
         self.fwd_btn.setIcon(fwd_icon)
+
+        re_icon_path = '/Users/stu/Documents/3D/QtDesigner/icons/BUTTONS' \
+                       '/reload.svg'
+        re_icon = QtGui.QIcon(re_icon_path)
+        self.re_btn.setIcon(re_icon)
 
         # create connections (/button functionality)
         self.set_proj.clicked.connect(self.set_project)
         self.back_btn.clicked.connect(self.back_button)
         self.fwd_btn.clicked.connect(self.forward_button)
         self.fwd_btn.clicked.connect(self.redo_click_forward)
+        self.re_btn.clicked.connect(self.refresh_current_scene_list)
         self.scene_list.doubleClicked.connect(self.double_click_forward)
 
         # Create layout (how widgets will be organised)
@@ -212,3 +220,6 @@ class ProjectManager(QtWidgets.QWidget):
             else:
                 print("    Can't go back any further!    ")
                 return
+
+    def refresh_current_scene_list(self):
+        self.update_scene_list()
